5e2a99f7f22d4e9ed8be9a9884d7a7bc32aba901
Efren Vasquez <efren.vasquez@wsu.edu>
2022-10-05 10:10:07 -0500

    Add video summary to individual video page

--- lib/classes/post-types/videos/includes/video.php
+++ lib/classes/post-types/videos/includes/video.php
@@ -1,1 +1,4 @@
-<div class="cahnrs-video-wrapper"><iframe width="560" height="315" src="<?php echo esc_url( $video_url ); ?>?rel=0" frameborder="0" allowfullscreen></iframe></div>
+<div class="cahnrs-video-wrapper">
+    <iframe width="560" height="315" src="<?php echo esc_url( $video_url ); ?>?rel=0" frameborder="0" allowfullscreen></iframe>
+    <p><?php echo wp_kses_post( $post->post_excerpt ); ?></p>
+</div>
