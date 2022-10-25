c0909953ff5ccdb0e2411f5b212504ddcd4b8e6b
Efren Vasquez <efren.vasquez@wsu.edu>
2022-10-25 14:33:01 -0500

    modal styling and functionality changes

--- assets/public/js/scripts.js
+++ assets/public/js/scripts.js
@@ -1,14 +1,14 @@
 // Stops YouTube video from playing whenever modal is closed
 (function($) {
-    $('.modal').each(function(){
-        var src = $(this).find('iframe').attr('src');
 
-        $(this).on('click', function(){
-            $(this).find('iframe').attr('src', '');
-            $(this).find('iframe').attr('src', src);
+    //Stops video on modal close
+    $(".modal").on('hidden.bs.modal', function (e) {
+        $(".modal iframe").attr("src", $(".modal iframe").attr("src"));
+    });
 
-        });
+    //Closes modal when button is clicked
+    $('.modal').on('click', 'button.close', function (eventObject) {
+        $('.modal').modal('hide');
     });
 })( jQuery );
-
 
